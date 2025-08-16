32e50ba752e3fe30644cfe2e1ed93e1159340ec7
Abdullah Ubaid <abdullahubaid1122@gmail.com>
2025-08-16 15:33:07 +1000

    navbar update
    Navbar is updated

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //baseURL: "http://localhost:5001", // local
-  baseURL: "http://52.65.234.130:5001", // live
+  baseURL: "http://localhost:5001", // local
+  //baseURL: "http://52.65.234.130:5001", // live
   headers: { "Content-Type": "application/json" },
 });
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import logo from "../assets/GoVibe.png";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
@@ -7,17 +8,26 @@ const Navbar = () => {
 
   const handleLogout = () => {
     logout();
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <Link to="/" className="text-2xl font-bold">Your apps name</Link>
+    <nav
+      className="text-white p-4 flex justify-between items-center h-20 bg-opacity-100"
+      style={{ backgroundColor: "#abc4cb" }}
+    >
+      <Link to="/" className="text-2xl font-bold">
+        <img src={logo} alt="GoVibe Logo" className="h-20 w-15" />
+      </Link>
       <div>
         {user ? (
           <>
-            <Link to="/tasks" className="mr-4">CRUD</Link>
-            <Link to="/profile" className="mr-4">Profile</Link>
+            <Link to="/tasks" className="mr-4">
+              CRUD
+            </Link>
+            <Link to="/profile" className="mr-4">
+              Profile
+            </Link>
             <button
               onClick={handleLogout}
               className="bg-red-500 px-4 py-2 rounded hover:bg-red-700"
@@ -27,10 +37,16 @@ const Navbar = () => {
           </>
         ) : (
           <>
-            <Link to="/login" className="mr-4">Login</Link>
+            <Link
+              to="/login"
+              className="mr-4 px-4 py-2 rounded-[10px] bg-blue-500 text-white border border-white hover:bg-blue-700"
+            >
+              Login
+            </Link>
+
             <Link
               to="/register"
-              className="bg-green-500 px-4 py-2 rounded hover:bg-green-700"
+              className="px-4 py-2 rounded-[10px] bg-green-500 text-white border border-white hover:bg-green-700"
             >
               Register
             </Link>
