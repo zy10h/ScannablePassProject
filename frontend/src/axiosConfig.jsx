3bea89f97305f0484bbac0c72f5275b040d63f2e
Abdullah Ubaid <abdullahubaid1122@gmail.com>
2025-08-18 16:51:03 +1000

    running on new instance

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   //baseURL: "http://localhost:5001/api",
-  baseURL: "http://52.65.234.130:5001/api", // live
+  baseURL: "http://13.239.3.141:5001/api", // live
   headers: { "Content-Type": "application/json" },
 });
 
