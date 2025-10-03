2023c11bbf8e6f6eaf6da71ec12c6eb0f62b81f1
zy10h <zy10h97@gmail.com>
2025-10-03 22:13:48 +1000

    12345

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://3.24.110.200:5001/api",
+  baseURL: "http://54.252.20.20:5001/api",
   //baseURL: "http://13.239.3.141:5001/api", // live
   headers: { "Content-Type": "application/json" },
 });
